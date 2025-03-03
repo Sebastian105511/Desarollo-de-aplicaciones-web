14c203994dc6602cda78bbc91c46861403391f23
Sebastian <sp.ordonezo@uea.edu.ec>
2025-03-02 20:37:31 -0500

    commit

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mi_clave_secreta'
 
-@app.route("/")
+# Definimos el formulario
+class MiFormulario(FlaskForm):
+    nombre = StringField('Nombre', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
+# Ruta de la página de inicio
+@app.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/about")
+# Ruta de la página 'about'
+@app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template('about.html')
+
+# Ruta para mostrar y procesar el formulario
+@app.route('/', methods=['GET', 'POST'])
+def formulario():
+    form = MiFormulario()
+    if form.validate_on_submit():
+        return render_template('resultado.html', nombre=form.nombre.data)
+    return render_template('formulario.html', form=form)
+
+# Ruta para mostrar el resultado después de enviar el formulario
+@app.route('/resultado', methods=['GET', 'POST'])
+def resultado():
+    return render_template('resultado.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
